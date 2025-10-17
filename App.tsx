94b0dc2858e788201e4a158dcd32143b35a90959
andretini <andrealijaramosagostini@gmail.com>
2025-10-16 21:28:57 -0300

    pages

--- App.tsx
+++ App.tsx
@@ -1,73 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
-import { Pokemon, PokemonListResult } from './models/pokemon_model';
-import { useEffect, useState } from 'react';
-import PokemonCard from './components/pokemon_card';
+import { StyleSheet } from 'react-native';
+import PokemonListPage from './pages/pokemon_list_page';
+import { NavigationContainer } from '@react-navigation/native';
 
-// Removed unused 'api' constant
+
+import * as React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const stack = createNativeStackNavigator();
 
 export default function App() {
-    const infinityScrollSize = 20
-
-    async function fetchPokemon(poke: PokemonListResult) {
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.Name}`);
-        const data = await resp.json();
-        const pokemon = Pokemon.fromJson(data);
-        return pokemon;
-    }
-    
-    async function fetchPokemons(offset: number) {
-        const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${infinityScrollSize}`);
-        const data = await resp.json();
-        const newPokemons = await Promise.all(
-            data.results.map(async (pokemon: any) => await fetchPokemon(PokemonListResult.fromJson(pokemon)))
-        );
-        setPokemon((pokemons) => [...pokemons, ...newPokemons]);
-    }
-
-    useEffect(() => {
-        setPokemon([])
-        fetchPokemons(0)
-        setOffset(infinityScrollSize)
-    }, []);
-
-    const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-    const [offset, setOffset] = useState(0)
-
     return (
-        <View style={styles.container}>
-            <FlatList
-            numColumns={2}
-            style={styles.list}
-            data={pokemon ? pokemon : []}
-            keyExtractor={(item) => item.name}
-            renderItem={({ item }) => <PokemonCard pokemon={item} />}
-            contentContainerStyle={{
-                paddingHorizontal: 10,
-            }}
-            columnWrapperStyle={{
-                justifyContent: "space-between",
-            }}
-
-            onEndReachedThreshold={0.5}
-            onEndReached={() => {
-                setOffset((prev) => prev + 20);
-                fetchPokemons(offset)
-            }}
-            />
-        </View>
-    );
+        <NavigationContainer>
+            <stack.Navigator>
+                <stack.Screen name="Home" component={PokemonListPage} />
+            </stack.Navigator>
+        </NavigationContainer>
+    )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    list: {
-        width: '100%',
-        backgroundColor: '#fff',
-    },
-});
